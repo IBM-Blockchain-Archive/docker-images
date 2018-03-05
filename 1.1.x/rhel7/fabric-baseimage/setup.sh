df475875f90b68543b623f0ef30508b92f6113ee
Gari Singh <mastersingh24@gmail.com>
2018-03-05 05:47:15 -0600

    Fix setup.sh
    Was not setting NODE_PKG properly
    
    Signed-off-by: Gari Singh <mastersingh24@gmail.com>

--- 1.1.x/rhel7/fabric-baseimage/setup.sh
+++ 1.1.x/rhel7/fabric-baseimage/setup.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
 GO_VER=1.9.2
 NODE_VER=8.9.4
-ARCH=`uname -m | sed 's|i686|x86|' | sed 's|x86_64|x64|'`
 
 # install Go
-cd /opt && curl https://dl.google.com/go/go${GO_VER}.linux-${ARCH}.tar.gz | tar xz
+ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
+echo "Installing Go ${GO_VER} for ${ARCH}"
+cd /opt && curl https://dl.google.com/go/go${GO_VER}.linux-${ARCH}.tar.gz | tar -xz
 
 # install Node.js
-cd /usr/local && curl https://nodejs.org/dist/v$NODE_VER/$NODE_PKG | tar --strip-components 1 xz
+ARCH=`uname -m | sed 's|i686|x86|' | sed 's|x86_64|x64|'`
+echo "Installing Node.js ${NODE_VER} for ${ARCH}"
+NODE_PKG=node-v$NODE_VER-linux-$ARCH.tar.gz
+cd /usr/local && curl https://nodejs.org/dist/v$NODE_VER/$NODE_PKG | tar --strip-components 1 -xz
