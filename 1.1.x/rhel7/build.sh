712c0a396fbf9b422d61e7cfede1fadc3e9489e1
Gari Singh <mastersingh24@gmail.com>
2018-03-05 11:01:25 -0600

    Add fabric-ca image
    Signed-off-by: Gari Singh <mastersingh24@gmail.com>

--- 1.1.x/rhel7/build.sh
+++ 1.1.x/rhel7/build.sh
@@ -22,16 +22,27 @@ echo -e "building peer docker\n"
 DOCKER_NS=ibmblockchain BASE_DOCKER_NS=ibmblockchain DOCKER_DYNAMIC_LINK=true make peer-docker
 echo -e "building orderer docker\n"
 DOCKER_NS=ibmblockchain BASE_DOCKER_NS=ibmblockchain DOCKER_DYNAMIC_LINK=true make orderer-docker
-# re tag images
+# build fabric-ca
+echo -e "building fabric-ca docker\n"
+cd ..
+git clone -b v${FABRIC_VER} https://github.com/hyperledger/fabric-ca
+cd fabric-ca
+# need to create hyperledger tag as fabric-ca does not have variabel in Dockerfile
+sed -i 's|RUN .*apt-get.*||g' images/fabric-ca/Dockerfile.in
+sed -i 's|hyperledger|ibmblockchain|g' images/fabric-ca/Dockerfile.in
+DOCKER_NS=ibmblockchain BASE_DOCKER_NS=ibmblockchain FABRIC_CA_DYNAMIC_LINK=true make docker-fabric-ca
+# re-tag images
 docker tag ibmblockchain/fabric-peer:${ARCH}-${FABRIC_VER} ibmblockchain/fabric-peer:${OS_TAG}-${FABRIC_VER}
 docker tag ibmblockchain/fabric-orderer:${ARCH}-${FABRIC_VER} ibmblockchain/fabric-orderer:${OS_TAG}-${FABRIC_VER}
 docker tag ibmblockchain/fabric-ccenv:${ARCH}-${FABRIC_VER} ibmblockchain/fabric-ccenv:${OS_TAG}-${FABRIC_VER}
 docker tag ibmblockchain/fabric-javaenv:${ARCH}-${FABRIC_VER} ibmblockchain/fabric-javaenv:${OS_TAG}-${FABRIC_VER}
+docker tag ibmblockchain/fabric-ca:${ARCH}-${FABRIC_VER} ibmblockchain/fabric-ca:${OS_TAG}-${FABRIC_VER}
 docker rmi ibmblockchain/fabric-baseimage:${ARCH}-${BASE_VER}
 docker rmi ibmblockchain/fabric-baseos:${ARCH}-${BASE_VER}
 docker rmi ibmblockchain/fabric-peer:${ARCH}-${FABRIC_VER}
 docker rmi ibmblockchain/fabric-orderer:${ARCH}-${FABRIC_VER}
 docker rmi ibmblockchain/fabric-ccenv:${ARCH}-${FABRIC_VER}
 docker rmi ibmblockchain/fabric-javaenv:${ARCH}-${FABRIC_VER}
+docker rmi ibmblockchain/fabric-ca:${ARCH}-${FABRIC_VER}
 cd ../../
 rm -Rf build
